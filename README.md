2f7f1cafb788bdfc88d5be4157c4fc58d37e09e3
wangmenghua <tyranny0303@163.com>
2018-10-25 10:54:25 +0800

    addNew2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # LimboChecker
+```NonNullCondition noNull = new NonNullCondition(false);
+ContentMatchBody name = new ContentMatchBody("name",editText1.getText().toString().trim());
+
+ContentMatchBody IDcard = new ContentMatchBody("idcard",editText2.getText().toString().trim());
+
+LinkedHashMap<TextInputLayout, LinkedHashMap<BaseCondition, ContentMatchBody>> mapHashMap = new LinkedHashMap<>();
+
+LinkedHashMap<BaseCondition, ContentMatchBody> map3 = new LinkedHashMap<>();
+map3.put(noNull, name);
+
+LinkedHashMap<BaseCondition, ContentMatchBody> map2 = new LinkedHashMap<>();
+map2.put(noNull, IDcard);
+
+mapHashMap.put(editText1, map3);
+mapHashMap.put(editText2, map2);
+
+OverAllUtil.checkAll(mapHashMap);
+```
--- limbochecker/src/main/java/com/tyranny/limbochecker/util/OverAllUtil.java
+++ limbochecker/src/main/java/com/tyranny/limbochecker/util/OverAllUtil.java
@@ -10,29 +10,6 @@ import java.util.Map;
 
 /**
  * Created by tyranny on 2018/9/10 11:06.
- * <p>
- * <p>
- * <p>
- * <p>
- * NonNullCondition noNull = new NonNullCondition(false);
- * ContentMatchBody name = new ContentMatchBody("name",
- * editText1.getText().toString().trim());
- * <p>
- * ContentMatchBody IDcard = new ContentMatchBody("idcard",
- * editText2.getText().toString().trim());
- * <p>
- * LinkedHashMap<TextInputLayout, LinkedHashMap<BaseCondition, ContentMatchBody>> mapHashMap = new LinkedHashMap<>();
- * <p>
- * LinkedHashMap<BaseCondition, ContentMatchBody> map3 = new LinkedHashMap<>();
- * map3.put(noNull, name);
- * <p>
- * LinkedHashMap<BaseCondition, ContentMatchBody> map2 = new LinkedHashMap<>();
- * map2.put(noNull, IDcard);
- * <p>
- * mapHashMap.put(editText1, map3);
- * mapHashMap.put(editText2, map2);
- * <p>
- * OverAllUtil.checkAll(mapHashMap);
  *
  * @version 1.0.0
  */
